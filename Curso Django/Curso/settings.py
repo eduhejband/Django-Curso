59b20b6e931a8d1ea75c7085e21584cf433b757c
Eduardo Hejazi Bandeira <eduardohejband@gmail.com.br>
2022-08-03 14:23:00 -0300

    Update curso

--- Curso Django/Curso/settings.py
+++ Curso Django/Curso/settings.py
@@ -33,19 +33,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #libs
-    'taggit',
-    # apps
+
     'simplemooc.core',
     'simplemooc.accounts',
     'simplemooc.courses',
-    'simplemooc.forum',
-)
-
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
-
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
 )
 
 MIDDLEWARE_CLASSES = (
